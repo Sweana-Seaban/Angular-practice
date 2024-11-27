3359a30ed798706bba446559e1a92533bdea5c86
SweanaSeaban <sweanaseaban@gmail.com>
2024-11-27 15:23:41 +0530

    feat: add formArray

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import {
+  FormArray,
+  FormBuilder,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
@@ -9,17 +12,27 @@ import {
 @Component({
   selector: 'app-profile-editor',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgFor],
   templateUrl: './profile-editor.component.html',
   styleUrl: './profile-editor.component.css',
 })
 export class ProfileEditorComponent {
+  private formBuilder = inject(FormBuilder);
   profileForm = new FormGroup({
     firstName: new FormControl('', Validators.required),
     lastName: new FormControl(''),
+    skills: new FormArray([
+      new FormControl(null),
+      new FormControl(null),
+      new FormControl(null),
+    ]),
   });
 
   onSubmit() {
     console.log('Form values', this.profileForm.value);
+  }
+
+  get skills(): FormArray {
+    return this.profileForm.get('skills') as FormArray;
   }
 }
